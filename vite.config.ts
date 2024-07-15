6d1cf6c946d297c50cef90081cad479a5914f533
dableeet <krasilnikov19@gmail.com>
2024-07-15 14:03:05 +0300

    added "in work" pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,10 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 import { ViteAliases } from 'vite-aliases';
 
-import profileData from './src/pages/profile/data';
 import loginData from './src/pages/login/data';
 import signInData from './src/pages/sign-in/data';
 
 const pageData = {
-  '/index.html': {
-    title: 'Main Page',
-  },
-  '/pages/profile/index.html': profileData,
   '/pages/login/index.html': loginData,
   '/pages/sign-in/index.html': signInData,
 };
@@ -43,6 +38,8 @@ export default defineConfig({
         index: resolve(__dirname, 'src/index.html'),
         login: resolve(__dirname, 'src/pages/login/index.html'),
         sign_in: resolve(__dirname, 'src/pages/sign-in/index.html'),
+        profile: resolve(__dirname, 'src/pages/profile/index.html'),
+        chat: resolve(__dirname, 'src/pages/chat/index.html'),
         '500': resolve(__dirname, 'src/pages/error/500/index.html'),
         '404': resolve(__dirname, 'src/pages/error/404/index.html'),
       },
