3ac9dc31469935e8871a21cdc1aa04b6f550cadb
dableeet <krasilnikov19@gmail.com>
2024-07-24 15:45:37 +0300

    profile page ready

--- src/pages/profile/data.ts
+++ src/pages/profile/data.ts
@@ -1,1 +1,117 @@
-export default {};
+const userData = {
+  name: 'Иван',
+  surname: 'Иванов',
+  email: 'pochta@yandex.ru',
+  login: 'ivanivanov',
+  chatName: 'Иван',
+  phone: '+7 (909) 967 30 30',
+};
+
+export default {
+  class: 'profile',
+  returnButton: {
+    type: 'button',
+    class: 'button button_with_arrow',
+    image: {
+      src: '../../assets/icons/rounded-arrow/rounded-arrow.svg',
+      alt: 'arrow',
+    },
+  },
+  avatarButton: {
+    type: 'button',
+    class: 'button button_avatar profile__avatar-btn',
+    image: {
+      class: 'button_avatar__image',
+      src: '../../assets/icons/avatar-circle/avatar.svg',
+      alt: 'avatar',
+    },
+  },
+  title: {
+    class: 'profile__username',
+    value: userData.name,
+  },
+  lists: [
+    {
+      class: 'list profile__user-data-list',
+      items: [
+        {
+          class: 'list__item',
+          title: {
+            class: 'list__item__title',
+            innerText: 'Почта',
+          },
+          value: { class: 'list__item__value', innerText: userData.email },
+        },
+        {
+          class: 'list__item',
+          title: {
+            class: 'list__item__title',
+            innerText: 'Логин',
+          },
+          value: { class: 'list__item__value', innerText: userData.login },
+        },
+        {
+          class: 'list__item',
+          title: {
+            class: 'list__item__title',
+            innerText: 'Имя',
+          },
+          value: { class: 'list__item__value', innerText: userData.name },
+        },
+        {
+          class: 'list__item',
+          title: {
+            class: 'list__item__title',
+            innerText: 'Фамилия',
+          },
+          value: { class: 'list__item__value', innerText: userData.surname },
+        },
+        {
+          class: 'list__item',
+          title: {
+            class: 'list__item__title',
+            innerText: 'Имя в чате',
+          },
+          value: { class: 'list__item__value', innerText: userData.chatName },
+        },
+        {
+          class: 'list__item',
+          title: {
+            class: 'list__item__title',
+            innerText: 'Телефон',
+          },
+          value: { class: 'list__item__value', innerText: userData.phone },
+        },
+      ],
+    },
+    {
+      class: 'list profile__button-group',
+      items: [
+        {
+          class: 'list__item',
+          button: {
+            class: 'button button_blue button_text-m',
+            type: 'button',
+            innerText: 'Изменить данные',
+          },
+        },
+        {
+          class: 'list__item',
+          button: {
+            class: 'button button_blue button_text-m',
+            type: 'button',
+            innerText: 'Изменить пароль',
+          },
+        },
+        {
+          class: 'list__item',
+          button: {
+            class: 'button button_red button_text-m',
+            type: 'button',
+            innerText: 'Выйти',
+          },
+        },
+      ],
+    },
+  ],
+};
--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -1,23 +1,25 @@
 import Handlebars from 'handlebars/runtime';
 
+import data from './data';
+
+import hashClasses from '@/utils/hash-classes';
+
+import profileLayout from './profile.layout.hbs';
+
 import button from '@partials/button.hbs';
 import arrow from '@assets/icons/rounded-arrow/rounded-arrow.hbs';
-import buttonWithArrow from '@partials/button-with-arrow.hbs';
+import list from '@partials/list.hbs';
 
 import styles from './styles.module.scss';
-import './index.scss';
 
-Handlebars.registerPartial({ arrow, button });
+Handlebars.registerPartial({ arrow, button, list });
+
+const moduledData = hashClasses({ ...data }, styles);
 
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('#container')!;
 
   container.classList.add(styles['container']);
 
-  container.innerHTML = container.innerHTML.concat(
-    buttonWithArrow({
-      class: `${styles['button']} ${styles['button_with_arrow']}`,
-      type: 'button',
-    }),
-  );
+  container.innerHTML = profileLayout(moduledData);
 });
